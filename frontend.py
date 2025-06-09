d87e88fc32989d5066dd4409a958768a1be28050
Leandro <leandropeppe@gmail.com>
2025-06-09 20:19:54 -0300

    Creacion rama Descifrado y agregado a frontend

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, messagebox, simpledialog
-from utils import cifrar_archivo
+from utils import cifrar_archivo, descifrar_archivo
 
 def seleccionar_y_cifrar():
     # Seleccionar archivo
@@ -25,14 +25,47 @@ def seleccionar_y_cifrar():
     except Exception as e:
         messagebox.showerror("Error", f"Error al cifrar el archivo:\n{str(e)}")
 
+
+def seleccionar_y_descifrar():
+    # Seleccionar archivo .enc
+    ruta_archivo = filedialog.askopenfilename(title="Selecciona un archivo para descifrar", filetypes=[("Archivos cifrados", "*.enc")])
+    if not ruta_archivo:
+        return
+
+    # Pedir clave
+    clave = simpledialog.askstring("Clave", "Introduce la clave original:", show='*')
+    if not clave:
+        messagebox.showerror("Error", "Debes introducir una clave.")
+        return
+
+    # Ruta del archivo descifrado (mismo directorio, sin extensión .enc)
+    directorio, nombre = os.path.split(ruta_archivo)
+    
+    if nombre.endswith(".enc"):
+        nombre_original = nombre[:-4]
+    else:
+        nombre_original = nombre + ".decrypted" 
+    
+    ruta_salida = os.path.join(directorio, nombre_original)
+
+    try:
+        descifrar_archivo(ruta_archivo, ruta_salida, clave)
+        messagebox.showinfo("Éxito", f"Archivo descifrado correctamente:\n{ruta_salida}")
+    except Exception as e:
+        messagebox.showerror("Error", f"Error al descifrar el archivo:\n{str(e)}")
+
+
 def main():
     root = tk.Tk()
-    root.title("Cifrador AES-256")
-    root.geometry("300x150")
+    root.title("Cifrador/Descifrador AES-256")
+    root.geometry("350x150") 
     root.resizable(False, False)
 
-    btn = tk.Button(root, text="Seleccionar archivo para cifrar", command=seleccionar_y_cifrar, padx=10, pady=10)
-    btn.pack(expand=True)
+    btnCifrado = tk.Button(root, text="Seleccionar archivo para CIFRAR", command=seleccionar_y_cifrar, padx=10, pady=10)
+    btnCifrado.pack(expand=True, fill='x', padx=20, pady=5)
+
+    btnDescrifrado = tk.Button(root, text="Seleccionar archivo para DESCIFRAR", command=seleccionar_y_descifrar, padx=10, pady=10)
+    btnDescrifrado.pack(expand=True, fill='x', padx=20, pady=5)
 
     root.mainloop()
 
